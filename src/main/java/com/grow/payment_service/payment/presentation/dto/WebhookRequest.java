53ad9affdc642f396fe917d3523551941c12d9db
wkdan <jmy8876@gmail.com>
2025-08-09 02:19:50 +0900

    ✨ feat: WebhookRequest 데이터 모델 확장 - 결제 정보 및 취소 내역 추가

--- src/main/java/com/grow/payment_service/payment/presentation/dto/WebhookRequest.java
+++ src/main/java/com/grow/payment_service/payment/presentation/dto/WebhookRequest.java
@@ -1,5 +1,7 @@
 package com.grow.payment_service.payment.presentation.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -26,10 +28,70 @@ public class WebhookRequest {
 	@NoArgsConstructor
 	@JsonIgnoreProperties(ignoreUnknown = true)
 	public static class WebhookData {
+		@JsonProperty("orderId")
 		private String orderId;
+
+		@JsonProperty("status")
 		private String status;
+
+		@JsonProperty("totalAmount")
 		private Integer amount;
+
+		@JsonProperty("currency")
+		private String currency;
+
+		@JsonProperty("method")
+		private String method;
+
+		@JsonProperty("requestedAt")
+		private String requestedAt;
+
+		@JsonProperty("approvedAt")
+		private String approvedAt;
+
+		@JsonProperty("easyPay")
+		private EasyPay easyPay;
+
+		@JsonProperty("cancels")
+		private List<Cancel> cancels;
+
+		@JsonProperty("customerEmail")
 		private String customerEmail;
+
+		@JsonProperty("customerName")
 		private String customerName;
+
+		@JsonProperty("receipt")
+		private Receipt receipt;
+
+		@Getter
+		@NoArgsConstructor
+		@JsonIgnoreProperties(ignoreUnknown = true)
+		public static class Receipt {
+			@JsonProperty("url")
+			private String url;
+		}
+
+		@Getter
+		@NoArgsConstructor
+		@JsonIgnoreProperties(ignoreUnknown = true)
+		public static class EasyPay {
+			@JsonProperty("provider")
+			private String provider;
+			@JsonProperty("amount")
+			private Integer amount;
+		}
+
+		@Getter
+		@NoArgsConstructor
+		@JsonIgnoreProperties(ignoreUnknown = true)
+		public static class Cancel {
+			@JsonProperty("cancelReason")
+			private String cancelReason;
+			@JsonProperty("cancelAmount")
+			private Integer cancelAmount;
+			@JsonProperty("canceledAt")
+			private String canceledAt;
+		}
 	}
 }
